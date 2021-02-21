9772c291a86e2bd2e79df4b178ab063237023b5d
firasbelhiba <firas.belhiba@esprit.tn>
2021-02-21 14:29:35 +0100

    User already exists exception

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator/check');
+const User = require('../../models/User');
 
 const router = express.Router();
 
@@ -19,13 +20,39 @@ router.post('/', [
     check('password', 'Please enter a password with 6 or more characters ').isLength({
         min: 6
     })
-], (req, res) => {
+], async (req, res) => {
     //console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+
+    try {
+
+        // See if user  exists 
+        let user = await User.findOne({ email });
+
+        if (user) {
+            res.status(400).json({ errors: [{ message: 'User already exists' }] });
+        }
+
+        // Get users gravatar 
+
+        // Encrypt password using bcrypt 
+
+        // Return the JWT using jsonwebtoken
+
+        res.send('User route');
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+
+
+
 });
 
 
