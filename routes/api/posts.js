d3470205109078b7d8e51d3de2a17ea8a4203df8
firasbelhiba <firas.belhiba@esprit.tn>
2021-02-26 19:29:14 +0100

    Get all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -37,4 +37,17 @@ router.post('/', [auth, [
     }
 });
 
+//@route GET api/posts
+//@desc Get all posts 
+//@access Private (you have to be logged in to create a post)
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+});
+
 module.exports = router; 
