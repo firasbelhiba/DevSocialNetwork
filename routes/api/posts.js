92e5313509b202efb27d2c7c8417274235829afb
firasbelhiba <firas.belhiba@esprit.tn>
2021-02-26 20:21:15 +0100

    Add a comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -145,4 +145,38 @@ router.put('/unlike/:id', auth, async (req, res) => {
     }
 });
 
+//@route POST api/posts/comment/:id
+//@desc Comment on a post 
+//@access Private (you have to be logged in to create a post)
+router.post('/comment/:id', [auth, [
+    check('text', 'Text must be required').not().isEmpty()
+
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await Post.findById(req.params.id);
+
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+
+        await post.save();
+        res.json(post.comments);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+});
+
 module.exports = router; 
