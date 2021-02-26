99967b269554fb22562ccb8e90918efed061a538
firasbelhiba <firas.belhiba@esprit.tn>
2021-02-26 19:23:28 +0100

    Add post Implementation

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 
-//@route GET api/posts
-//@desc Test route 
-//@access Public
-router.get('/', (req, res) => res.send('Posts route'));
+
+//@route POST api/posts
+//@desc Create a post 
+//@access Private (you have to be logged in to create a post)
+router.post('/', [auth, [
+    check('text', 'Text must be required').not().isEmpty()
+
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: req.body.avatar,
+            user: req.user.id
+        });
+        const post = await newPost.save();
+        res.json(post);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+});
 
 module.exports = router; 
