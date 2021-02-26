8a9cc3d37ac6b7cd18fcc642107cc20982371ac4
firasbelhiba <firas.belhiba@esprit.tn>
2021-02-26 20:09:42 +0100

    Unlike Post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -117,4 +117,32 @@ router.put('/like/:id', auth, async (req, res) => {
     }
 });
 
+//@route PUT api/posts/unlike/:id
+//@desc Unlike a post  
+//@access Private (you have to be logged in to create a post)
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // checking if the post is already liked by this user 
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ message: 'Post has not yet been liked' });
+        }
+
+        // Remove index 
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
+
+        await post.save();
+        res.json(post.likes);
+    } catch (error) {
+        console.error(error.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ message: 'Post not found' });
+        }
+        res.status(500).send('Server error');
+    }
+});
+
 module.exports = router; 
