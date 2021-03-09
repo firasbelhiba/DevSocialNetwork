d3554e2aca35cccdd46f9a21f2a7c4c359f0ad28
firasbelhiba <firas.belhiba@esprit.tn>
2021-03-09 02:17:06 +0100

    UI test consuming web service with axios (Success)

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 
 export const Register = () => {
@@ -14,12 +15,36 @@ export const Register = () => {
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
             console.log('Passwords do not match');
         } else {
-            console.log(formData);
+            // const newUser = {
+            //     name,
+            //     email,
+            //     password
+            // }
+            // try {
+            //     const config = {
+            //         headers: {
+            //             'Content-Type': 'application/json'
+            //         }
+            //     }
+            //     const body = JSON.stringify(newUser);
+
+            //     const res = await axios.post('http://localhost:5000/api/users', body, config);
+            //     console.log(res.data);
+            //     // const response = await axios({
+            //     //     method: 'post',
+            //     //     url: 'http://localhost:5000/api/users',
+            //     //     data: body,
+            //     //     headers: config
+            //     // });
+            //     // console.log(response.data); 
+            // } catch (err) {
+            //     console.error(err.response.data);
+            // }
         }
     }
 
