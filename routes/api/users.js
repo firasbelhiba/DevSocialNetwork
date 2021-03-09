9e0ee79a4cab22c21b9f8a301b8afce332d0d061
firasbelhiba <firas.belhiba@esprit.tn>
2021-03-09 02:18:09 +0100

    Allow Control access

--- routes/api/users.js
+++ routes/api/users.js
@@ -92,4 +92,4 @@ router.post('/', [
 });
 
 
-module.exports = router; 
+module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,26 @@ app.get('/', (req, res) => res.send('API Running'));
 //Init middleware (Body Parser , now it s included with express )
 app.use(express.json({ extended: false }));
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
@@ -22,6 +42,8 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 
 
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
